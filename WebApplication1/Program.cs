a1c8dd6d25b5f1a9dcd7074fb9f6b9cb2f97a4b8
Devendra Korde <devendra.korde@rib-software.com>
2025-06-25 15:14:42 +0530

    Add .gitignore and exclude .vs folder

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddCors(options => { 
+    options.AddPolicy("AllowReactApp", policy => policy.WithOrigins("http://localhost:3000")
+    .AllowAnyMethod()
+    .AllowAnyHeader()); 
+});
+var app = builder.Build();
+
+app.UseCors("AllowReactApp");
+
+app.MapGet("/todoitems",async (TodoDb db) =>
+    await db.Todos.ToListAsync());
+
+app.MapGet("/gettodoitem/{id}", async (int id, TodoDb db) => 
+    await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
+
+app.MapPost("/createtodoitem", async (Todo todo,TodoDb db)=>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    return Results.Created();
+});
+app.MapPut("/updatetodoitem", async (Todo todo, TodoDb db)=>
+{
+    db.Todos.Update(todo);
+    await db.SaveChangesAsync();
+});
+
+app.Run();
