4c97ff7ce42fab9801a0a715c8dea25e22b1503d
Devendra Korde <devendra.korde@rib-software.com>
2025-06-26 17:02:06 +0530

    gitignore file updated

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee13455f869d447ab68bf30c81885aee734d7c33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918e220ffda38a908e79dfdbe1da03c47fc2ef48")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
