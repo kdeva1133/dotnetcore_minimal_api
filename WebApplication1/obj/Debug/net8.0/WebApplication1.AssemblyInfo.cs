168825571d2ae8a8a6b82d780810e7c9ca9086fc
Devendra Korde <devendra.korde@rib-software.com>
2025-06-26 16:38:39 +0530

    Revert "gitingore file updated and origin changed again"
    This reverts commit a28b6f404e0e76cf091012c80ed97079f7caf1c4.

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee13455f869d447ab68bf30c81885aee734d7c33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da813243b091f7251b56dd753a0779f8b4d28e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
